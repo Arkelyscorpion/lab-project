fa606801effc3d3faadf830f76bc0efb297c8477
rishiyanth <apprishiategood002@gmail.com>
2023-01-20 15:56:24 +0530

    2. form validation

--- login.js
+++ login.js
@@ -1,6 +1,13 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
+const signUpBtn = document.getElementById('signupbtn');
+const signInBtn = document.getElementById('signinbtn');
 const container = document.getElementById('container');
+const signupName = document.getElementById('signupname');
+const signupEmail = document.getElementById('signupemail');
+const signupPassword = document.getElementById('signuppassword');
+const loginEmail = document.getElementById('loginemail');
+const loginPassword = document.getElementById('loginpassword');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -9,3 +16,17 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+var dict = {}
+
+function validateSignUp(){
+	dict[signupEmail.value] = signupPassword.value;
+	console.log(dict); 	
+}
+
+function validateLogin(){
+	if(dict[loginEmail.value] === loginPassword.value)
+		alert("Success");
+	else
+		alert("retry");
+}
