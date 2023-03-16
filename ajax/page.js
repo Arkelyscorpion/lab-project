ef4d09c10e4b6262a6f50e447dd86a65e269d6aa
Krishna Sameer <krishnasameer51@gmail.com>
2023-03-16 23:56:17 +0530

    added servlet

--- ajax/page.js
+++ ajax/page.js
@@ -1,8 +1,16 @@
-function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      document.getElementById("demo").innerHTML = this.responseText;
-      }
-    xhttp.open("GET", "ajax.txt", true);
-    xhttp.send();
+function loadDoc(){
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "file.xml",false);
+  xhttp.send();
+  const xmlDoc = xhttp.responseXML;
+  console.log(xmlDoc);
+  const x = xmlDoc.getElementsByTagName("ARTIST");
+  let txt = "";
+  for (let i = 0; i < x.length; i++) {
+    txt += x[i].childNodes[0].nodeValue + "<br>";
+    console.log(x[i].childNodes[0]);
   }
+  // document.getElementById("demo").innerHTML = txt;
+  document.getElementById("demo").innerHTML =
+    xhttp.getAllResponseHeaders();
+}
